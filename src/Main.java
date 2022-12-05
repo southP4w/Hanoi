38d56b89e884c09dc7f0de8c08998e35a0b230fb
southpaw <dangargiullo@gmail.com>
2022-12-05 06:25:40 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+public class Main
+{
+	public static void main(String[] args) {
+		hanoi(4, 'A', 'B', 'C');
+	}
+
+	private static void hanoi(int n, char pegFrom, char pegMid, char pegTo) {
+		if (n==1) {
+			System.out.println("Disk 1 moved from peg " + pegFrom + " to peg " + pegTo);
+			return;
+		}
+		hanoi(n-1, pegFrom, pegTo, pegMid);
+		System.out.println("Disk " + n + " moved from peg " + pegFrom + " to peg " + pegTo);
+		hanoi(n-1, pegMid, pegFrom, pegTo);
+	}
 }
